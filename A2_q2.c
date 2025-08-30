0c2ce2b4c1fa4927c7d924cb081de473faaa9146
Sampadadandekar <144693852+Sampadadandekar@users.noreply.github.com>
2025-08-30 12:59:43 +0530

    Add files via upload

---
+++ A2_q2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+    int a,b,c;
+    printf("Enter two no. : .\n");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a>b && a>c)
+        printf("%d is largest", a);
+    else if (b>c)
+      printf("%d is largest", b);
+}
