7a0459765230c304b99b1b978b7a36800fc6fafd
Sampadadandekar <144693852+Sampadadandekar@users.noreply.github.com>
2025-08-30 13:12:18 +0530

    Add files via upload

---
+++ A2_q4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+
+    printf("Enter an integer: ");
+    scanf("%d", &a);
+
+    if (a% 7 == 0)
+    {
+        printf("%d is divisible by 7.\n", a);
+    }
+    else {
+        printf("%d is not divisible by 7.\n", a);
+    }
+
+    return 0;
+}
